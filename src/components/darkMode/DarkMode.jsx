4803130f759faff2ab4b2367411c1cb78a6d7d5a
André Marques <andrelcm89pt@gmail.com>
2023-06-20 14:46:34 +0100

    adding new stuff

--- src/components/darkMode/DarkMode.jsx
+++ src/components/darkMode/DarkMode.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+"use client";
+
+import React, { useContext } from "react";
 import styles from "./darkMode.module.css";
+import { ThemeContext } from "../../../context/ThemeContext";
 
 const DarkMode = () => {
+  const { toggle, mode } = useContext(ThemeContext);
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🔆</div>
-      <div className={styles.button} />
+      <div
+        className={styles.button}
+        style={mode === "dark" ? { right: "2px" } : { left: "2px" }}
+      />
     </div>
   );
 };
