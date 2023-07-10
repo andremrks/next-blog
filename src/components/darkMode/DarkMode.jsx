70ed79c0ae23ac7844e695468f973d2bb3af8e2a
André Marques <andrelcm89pt@gmail.com>
2023-07-10 10:27:48 +0100

    creating api

--- src/components/darkMode/DarkMode.jsx
+++ src/components/darkMode/DarkMode.jsx
@@ -2,7 +2,7 @@
 
 import React, { useContext } from "react";
 import styles from "./darkMode.module.css";
-import { ThemeContext } from "../../../context/ThemeContext";
+import { ThemeContext } from "../../context/ThemeContext";
 
 const DarkMode = () => {
   const { toggle, mode } = useContext(ThemeContext);
